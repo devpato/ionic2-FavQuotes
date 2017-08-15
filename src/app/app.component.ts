81108dcddcf5b9ede8f5a04d8beb07bfad3e35cf
Patricio <pevargas@valdosta.edu>
2017-08-15 00:34:54 -0400

    creating all the pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
+import { FavoritesPage } from './../pages/favorites/favorites';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = FavoritesPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,8 @@
+import { SettingsPage } from './../pages/settings/settings';
+import { QuotesPage } from './../pages/quotes/quotes';
+import { QuotePage } from './../pages/quote/quote';
+import { LibraryPage } from './../pages/library/library';
+import { FavoritesPage } from './../pages/favorites/favorites';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,12 +10,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    LibraryPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +28,11 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    LibraryPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
