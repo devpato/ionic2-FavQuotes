ad1c27e6a3ab7767a2e65551e299923af3f18793
Patricio <pevargas@valdosta.edu>
2017-08-15 22:02:07 -0400

    dispaly quotes categories and create quote object

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,25 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Quote} from "../../data/quote.interface";
+import quotes from "../../data/quotes";
 
-/**
- * Generated class for the LibraryPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
-export class LibraryPage {
+export class LibraryPage implements OnInit {
+  quoteCollection: {category: string, quotes: Quote[], icon: string}[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  ngOnInit(){
+    this.quoteCollection = quotes;
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LibraryPage');
-  }
-
 }
