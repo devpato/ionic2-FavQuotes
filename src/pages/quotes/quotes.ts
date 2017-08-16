e81d9185ba0d822d8a900337daa66c3e16cd97b6
Patricio <pevargas@valdosta.edu>
2017-08-15 23:42:35 -0400

    function to add, remove and get quotes favs

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 import { Quote } from "../../data/quote.interface";
 @Component({
   selector: 'page-quotes',
@@ -7,7 +7,7 @@ import { Quote } from "../../data/quote.interface";
 })
 export class QuotesPage implements OnInit {
   quoteGroup: {category: string, quotes: Quote[], icon: string}
-  constructor( private navParams: NavParams){}
+  constructor( private navParams: NavParams,private alertCtrl: AlertController){}
     
     ngOnInit() {
       this.quoteGroup = this.navParams.data;
@@ -18,5 +18,27 @@ export class QuotesPage implements OnInit {
       //Add elvis operator (?)
     }*/
     
+    onAddToFavorite(selectedQuote:Quote) {//Alert on Ionic Doc
+      const alert = this.alertCtrl.create({
+        title:'Add Quote',
+        subTitle: 'Are you sure?',
+        message: 'Are yo sure you want to add the quote to Favorites?',
+        buttons: [
+          {
+            text: 'Yes',
+            handler: ()=> {
+              console.log("okay");
+            }
+          },
+          {
+            text: 'No',
+            handler: ()=> {
+              console.log("Oops!");
+            }
+          }
+        ]
+      });
+      alert.present();
+    }
 
 }
---
+++ src/services/quotes.ts
@@ -0,0 +1,22 @@
+import { Quote } from './../data/quote.interface';
+
+
+export class QuotesService {
+    private favoriteQuotes: Quote[] = [];  
+
+    addQuoteToFavorites(quote: Quote) {
+        this.favoriteQuotes.push(quote);
+    }
+
+    removeQuoteFromFavorites(quote: Quote) {
+        const position = this.favoriteQuotes.findIndex((quoteEl: Quote)=>{
+            return quoteEl.id == quote.id;
+        });
+        this.favoriteQuotes.splice(position,1);
+    }
+
+    getFavoriteQuotes() {
+        return this.favoriteQuotes.slice();
+    }
+
+}
