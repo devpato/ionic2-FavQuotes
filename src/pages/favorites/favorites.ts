95b9ad99fb8b82fa66721f533932275d91d46e09
Patricio <pevargas@valdosta.edu>
2017-08-16 00:06:44 -0400

    opening modal for quote

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,7 +1,8 @@
+import { QuotePage } from './../quote/quote';
 import { Quote } from './../../data/quote.interface';
 import { QuotesService } from './../../services/quotes';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-favorites',
@@ -9,10 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FavoritesPage {
   quotes: Quote[];
-  constructor(public quotesService: QuotesService) {}
+  constructor(public quotesService: QuotesService,
+                     private modalCtrl: ModalController
+  ) {}
 
   ionViewWillEnter() { //this will work even if the page is cached
     this.quotes = this.quotesService.getFavoriteQuotes();
   }
 
+  onViewQuote(quote: Quote) {
+    const modal = this.modalCtrl.create(QuotePage);
+    modal.present();
+  }
 }
